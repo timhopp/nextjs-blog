e0100ff3dbee253a9dbfef131aa71a858b52d582
tim hopp <timothyjhopp@gmail.com>
2020-10-13 14:24:11 -0600

    added styling and post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,22 @@
+import Head from "next/head";
 import Link from "next/link";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <main>
-      <h1 className="title">
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </h1>
-      <h1>First Post</h1>;{" "}
-    </main>
+    <Layout>
+      <main>
+        <Head>
+          <title>First Post</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <h1 className="title">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </h1>
+        <h1>First Post</h1>{" "}
+      </main>
+    </Layout>
   );
 }
