15669faafd7ed6b4999e81359fd57fd0afb9994f
tim hopp <timothyjhopp@gmail.com>
2020-10-21 16:20:44 -0600

    improved front page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 // @ts-ignore
 import utilStyles from "../styles/utils.module.css";
-import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
 import NavBarComp from "../components/nav";
@@ -15,61 +14,80 @@ export default function Home({ sportsnews, trendingnews, worldnews }) {
         <NavBarComp></NavBarComp>
       </div>
       <Head>
-        <title>{siteTitle}</title>
+        <title>News-Fi</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>
-          Hi, I'm Tim Hopp. This is a sample blog to learn React and Next.js.
-        </p>
+        <b className="ml-4">
+          Welcome to News-Fi, A sample news blog built with React and Next.js
+        </b>
       </section>
       <div className="container-fluid">
-        <div className="row">
-          <div className="col-4">
-            <div className="row">
-              {sportsnews.articles.map((sports) => (
-                <div className="row m-4 bg-light p-3 rounded">
-                  {/* <b key={sports.url}>{sports.source.name}</b> */}
-                  <div className="col-3">
-                    <img
-                      className={utilStyles.img}
-                      src={sports.urlToImage}
-                    ></img>
-                  </div>
-                  <Link href={sports.url}>
-                    <div className="col-9">
-                      <b className="text-center" key={sports.url}>
-                        {sports.title}
-                      </b>
-                      {/* <b>{sports.source.name}</b> */}
-                      <p>{sports.description}</p>
-                    </div>
-                  </Link>
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className="col-4 bg-dark">
-            {trendingnews.articles.map((trending) => (
-              <div className="row m-4 bg-light p-3 rounded">
-                {/* <b key={trending.url}>{trending.source.name}</b> */}
-                <div className="col-3">
-                  <img
-                    className={utilStyles.img}
-                    src={trending.urlToImage}
-                  ></img>
-                </div>
-                <Link href={trending.url}>
-                  <div className="col-9">
-                    <b className="text-center" key={trending.url}>
-                      {trending.title}
-                    </b>
-                    {/* <b>{trending.source.name}</b> */}
-                    <p>{trending.description}</p>
-                  </div>
-                </Link>
+        <div className="row justify-content-center">
+          <Link href="trendingUS">
+            <h3 className="text-left col-9">US News</h3>
+          </Link>
+
+          {trendingnews.articles.map((trending) => (
+            <div className="col-3 m-2 bg-light p-3 rounded">
+              {/* <b key={trending.url}>{trending.source.name}</b> */}
+              <div className="">
+                <img className={utilStyles.img} src={trending.urlToImage}></img>
               </div>
-            ))}
-          </div>
+              <Link href={trending.url}>
+                <div className="">
+                  <b className="text-center" key={trending.url}>
+                    {trending.title}
+                  </b>
+                  {/* <b>{trending.source.name}</b> */}
+                  <p>{trending.description}</p>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+        <div className="row justify-content-center">
+          <Link href="trendingWorld">
+            <h3 className="text-left col-9">World News</h3>
+          </Link>
+
+          {worldnews.articles.map((world) => (
+            <div className="col-3 m-2 bg-light p-3 rounded">
+              {/* <b key={world.url}>{world.source.name}</b> */}
+              <div className="">
+                <img className={utilStyles.img} src={world.urlToImage}></img>
+              </div>
+              <Link href={world.url}>
+                <div className="">
+                  <b className="text-center" key={world.url}>
+                    {world.title}
+                  </b>
+                  {/* <b>{world.source.name}</b> */}
+                  <p>{world.description}</p>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+        <div className="row justify-content-center">
+          <Link href="/sports">
+            <h3 className="text-left col-9">Sports</h3>
+          </Link>
+          {sportsnews.articles.map((sports) => (
+            <div className="col-3 m-2 bg-light p-2 rounded">
+              <div className="mb-2">
+                <img className={utilStyles.img} src={sports.urlToImage}></img>
+              </div>
+              <Link href={sports.url}>
+                <div className="">
+                  <b className="text-center" key={sports.url}>
+                    {sports.title}
+                  </b>
+                  {/* <b>{sports.source.name}</b> */}
+                  <p>{sports.description}</p>
+                </div>
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
     </div>
@@ -78,15 +96,15 @@ export default function Home({ sportsnews, trendingnews, worldnews }) {
 
 export async function getStaticProps() {
   const res = await fetch(
-    "https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=22221f62f9584a0d8654a29cadc834a8"
+    "https://newsapi.org/v2/top-headlines?country=us&pageSize=3&category=sports&apiKey=22221f62f9584a0d8654a29cadc834a8"
   );
   const sportsnews = await res.json();
   const res2 = await fetch(
-    "https://newsapi.org/v2/top-headlines?country=us&apiKey=22221f62f9584a0d8654a29cadc834a8"
+    "https://newsapi.org/v2/top-headlines?country=us&pageSize=3&apiKey=22221f62f9584a0d8654a29cadc834a8"
   );
   const trendingnews = await res2.json();
   const res3 = await fetch(
-    "https://newsapi.org/v2/everything?q=world&apiKey=22221f62f9584a0d8654a29cadc834a8"
+    "https://newsapi.org/v2/everything?q=world&pageSize=3&apiKey=22221f62f9584a0d8654a29cadc834a8"
   );
   const worldnews = await res3.json();
 
